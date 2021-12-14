b95f208619cbd7e75acc8a9466587f7650d72f36
pharaoh <pharaohmanson@gmail.com>
2021-12-13 20:34:19 -0500

    djoser

--- backend/flight_project/settings.py
+++ backend/flight_project/settings.py
@@ -136,6 +136,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 AUTH_USER_MODEL = 'flight_app.UserAccount'
 
 REST_FRAMEWORK = {
