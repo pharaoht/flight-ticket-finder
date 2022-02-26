c020b893d92a9482de997c5d69db65761c5b141b
pharaoh <pharaohmanson@gmail.com>
2022-02-26 11:49:58 -0500

    search flights

--- frontend/src/components/DestinationComponent/DestComponent.jsx
+++ frontend/src/components/DestinationComponent/DestComponent.jsx
@@ -1,14 +1,64 @@
-import React from 'react'
-import '../DestinationComponent/Destinations.css';
+import React, { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
+import ClipLoader from "react-spinners/ClipLoader";
+import '../DestinationComponent/DestComponent.css';
 
 const DestComponent = (props) => {
+    const [photoLink, setphotoLink] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const location = `${props.name}, ${props.country}`
+
+    const obj = {
+        method: 'GET',
+        redirect: 'follow',
+        url: `https://api.unsplash.com/search/photos?query=${props.name}&per_page=1&orientation=portrait&page=1&client_id=cKakzKM1cx44BUYBnEIrrgN_gnGqt81UcE7GstJEils`
+    };
+
+    const fetchPhotos = async (obj) => {
+        let link = await axios.get(obj.url, { method: obj.method, redirect: obj.redirect })
+            .then(res => { return res.data.results[0].urls.full })
+            .catch(err => { return console.log(err) });
+
+        return link;
+    };
+
+    const loadHandler = () => {
+        setIsLoaded(prev => true)
+    };
+
+    useEffect(async () => {
+        const fetchData = async () => {
+            let photo = await fetchPhotos(obj);
+            setphotoLink(photo);
+        };
+
+        fetchData();
+    }, []);
+
     return (
-        <div className='dest-comp-holder'>
+        <div className='dest-comp-holder' key={props.id}>
             <div className='dest-comp-photo'>
-                photo
+                <img className='img-dest' src={photoLink} onLoad={() => loadHandler()} alt='destination_photo' height='100%' width='200px' />
             </div>
             <div className='dest-comp-info'>
-                info
+                <div className='dest-info-header'>
+                    <h2>{location}</h2>
+                </div>
+                <div className='dest-comp-body'>
+                    <div>
+                        <h3>Information</h3>
+                    </div>
+                    <div className='dest-comp-sub-body'>
+                        <div className='sub-body-container'>
+                            <div className='sub-body-item'>1</div>
+                            <div className='sub-body-item'>2</div>
+                        </div>
+                        <div className='sub-body-container'>
+                            <div className='sub-body-item'>3</div>
+                            <div className='sub-body-item'>4</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- frontend/src/containers/Destinations/Destinations.jsx
+++ frontend/src/containers/Destinations/Destinations.jsx
@@ -6,7 +6,6 @@ import DestComponent from '../../components/DestinationComponent/DestComponent';
 
 const Destinations = () => {
     const [destinations, setDestinations] = useState([]);
-    const [change, setChange] = useState(false);
 
     const obj = {
         method: 'GET',
@@ -18,59 +17,31 @@ const Destinations = () => {
         url: 'https://tequila-api.kiwi.com/locations/topdestinations?term=london_gb&locale=en-US&limit=10&sort=name&active_only=true&source_popularity=searches',
     };
 
-    const getPhotos = async () => {
-
-        let arr = []
-        await fetchPhotos();
-        let newDest = combineLinks();
-        setDestinations(newDest);
-
-        async function fetchPhotos(obj) {
-            let link;
-            for (let i = 0; i < destinations.length; i++) {
-                const obj = {
-                    method: 'GET',
-                    redirect: 'follow',
-                    url: `https://api.unsplash.com/search/photos?query=${destinations[i].name}&per_page=1&orientation=portrait&page=1&client_id=cKakzKM1cx44BUYBnEIrrgN_gnGqt81UcE7GstJEils`
-                };
-
-                link = await axios.get(obj.url, { method: obj.method, redirect: obj.redirect })
-                    .then(res => { return res.data.results[0].urls.full })
-                    .catch(err => { return console.log(err) });
-                arr.push(link)
-            }
-        }
-
-        function combineLinks() {
-            return destinations.map((itm, idx) => {
-                itm['photo'] = arr[idx]
-                return itm;
-            });
-        };
-
-    };
-
     const { isLoading, error, sendRequest } = useHttp(obj, setDestinations);
 
-    const showDestinations = () => { return (destinations.map(item => { return (<div key={item.id}> {item.name} <img src={item.photo} alt='photo' weight='100px' height='100px' /> </div>) })) }
+    const showDestinations = () => {
+        return (
+            destinations.map(item => {
+                return (
+                    <DestComponent key={item.id} name={item.name} country={item.country.name} population={item.population} tags={item.tags} />
+                )
+            })
+        )
+    }
 
     useEffect(async () => {
-        await sendRequest();
-        setChange(true);
+        sendRequest();
     }, []);
-
-    useEffect(() => {
-        getPhotos()
-    }, [change])
 
     return (
         <div className='destination-parent'>
             <div className='destination-header'>
                 <h1>Top 10 Destinations</h1>
             </div>
-            <div>
-                <DestComponent />
+            <div className='destination-holder'>
+                {destinations.length > 1 ? showDestinations() : <div>No destinations...</div>}
             </div>
+
         </div>
     )
 }
